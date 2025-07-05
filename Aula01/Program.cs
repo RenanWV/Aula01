284a37573f75116203398118d044d71a521f0c29
Reena <Reena@5700X3D>
2025-07-04 21:22:43 -0300

    alterando o código de forma correta do camel case

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -1,67 +1,73 @@
-﻿const string NickName = "Kazumii";
-const int PercentualSucesso = 892; // 0.0892% de chance de sucesso
-const int MaxDurabilidade = 180;
-const int DurabilidadePerdida = 20;
-const int ValorEstilhaco = 5;
-const int CronsNecessarias = 4000;
-const int ValorCron = 3;
-const int PassarGarantido = 1000;
-const int MemoriasNecessarias = 1; 
+﻿const string nickName = "Kazumii";
+const int percentualSucesso = 892; // 0.0892% de chance de sucesso
+const int maxDurabilidade = 180;
+const int durabilidadePerdida = 20;
+const int valorEstilhaco = 5;
+const int cronsNecessarias = 4000;
+const int valorCron = 3;
+const int passarGarantido = 1000;
+const int memoriasNecessarias = 1; 
 
 // Estoque inicial 
-int estoqueCrons = 400000;
+int estoqueCrons = 42200000;
 int estoqueMemorias = 500;
 
 var random = new Random();
 int profit = random.Next(1, 1000000);
 int tentativas = 0;
-int durabilidadeAtual = MaxDurabilidade;
+int durabilidadeAtual = maxDurabilidade;
 int totalEstilhacosUsados = 0;
 int totalCronsUsadas = 0;
 int totalMemoriasUsadas = 0;
 bool passou = false;
     
-while (profit > PercentualSucesso && tentativas < PassarGarantido)
+while (profit > percentualSucesso && tentativas < passarGarantido)
 {
     // Verifica se tem crons e memorias suficientes
-    if (estoqueCrons < CronsNecessarias || estoqueMemorias < MemoriasNecessarias)
+    if (estoqueCrons < cronsNecessarias )
     {
-        Console.WriteLine("Faltou crons ou memórias do artesão! Contrate um CHINA.");
+        Console.WriteLine("Tá sem cron dog, vai grindar mendigo.");
+        break;
+    }
+    if (estoqueMemorias < memoriasNecessarias)
+    {
+        Console.WriteLine("Tá sem memória, vai farmar mendigo.");
         break;
     }
 
     tentativas++;
-    durabilidadeAtual -= DurabilidadePerdida;
-    totalCronsUsadas += CronsNecessarias;
-    totalMemoriasUsadas += MemoriasNecessarias;
-    estoqueCrons -= CronsNecessarias;
-    estoqueMemorias -= MemoriasNecessarias;
+    durabilidadeAtual -= durabilidadePerdida;
+    totalCronsUsadas += cronsNecessarias;
+    totalMemoriasUsadas += memoriasNecessarias;
+    estoqueCrons -= cronsNecessarias;
+    estoqueMemorias -= memoriasNecessarias;
 
-    Console.WriteLine($"{PercentualSucesso} de {profit} - Tentativa número {tentativas}, Durabilidade {durabilidadeAtual}");
+    Console.WriteLine($"{percentualSucesso} de {profit} - Tentativa número {tentativas}, Durabilidade {durabilidadeAtual}");
     Console.WriteLine("Rodou amigão!");
 
     profit = random.Next(1, 1000000);
 
-    if (durabilidadeAtual < DurabilidadePerdida)
+    if (durabilidadeAtual < durabilidadePerdida)
     {
         Console.WriteLine("Durabilidade insuficiente para continuar. Reparando no ferreiro");
-        int reparos = MaxDurabilidade - durabilidadeAtual;
-        durabilidadeAtual = MaxDurabilidade;
+        int reparos = maxDurabilidade - durabilidadeAtual;
+        durabilidadeAtual = maxDurabilidade;
         totalEstilhacosUsados += reparos;
         Console.WriteLine($"Durabilidade restaurada para {durabilidadeAtual}");
     }
 }
 
-if (tentativas == PassarGarantido)
+
+if (tentativas == passarGarantido)
 {
-    Console.WriteLine("Se fudeu, passou na bigorna");
+    Console.WriteLine("Se fudeu, passou no pit");
     passou = true;
 }
 
 // Mensagem final caso tenha passado
 if (passou)
 {
-    long totalGasto = (totalEstilhacosUsados * ValorEstilhaco) + (totalCronsUsadas * ValorCron);
-    Console.WriteLine($"{PercentualSucesso} de {profit} - Tentativa número {tentativas}");
-    Console.WriteLine($"{NickName} PASSOU! Total de estilhaços: {totalEstilhacosUsados} - Total de crons: {totalCronsUsadas} - Total de memórias: {totalMemoriasUsadas} - Total gasto: {totalGasto:N0}kk");
+    long totalGasto = (totalEstilhacosUsados * valorEstilhaco) + (totalCronsUsadas * valorCron);
+    Console.WriteLine($"{percentualSucesso} de {profit} - Tentativa número {tentativas}");
+    Console.WriteLine($"{nickName} PASSOU! Total de estilhaços: {totalEstilhacosUsados} - Total de crons: {totalCronsUsadas} - Total de memórias: {totalMemoriasUsadas} - Total gasto: {totalGasto:N0}kk");
 }
